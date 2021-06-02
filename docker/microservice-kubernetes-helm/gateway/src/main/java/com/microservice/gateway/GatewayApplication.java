32b51dc0bae5d44027de3bfaac22dee13c849a53
Rahul Kumar <30971078+rahulkumarsahu@users.noreply.github.com>
2021-06-02 16:20:14 +0530

    Add files via upload

---
+++ docker/microservice-kubernetes-helm/gateway/src/main/java/com/microservice/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
