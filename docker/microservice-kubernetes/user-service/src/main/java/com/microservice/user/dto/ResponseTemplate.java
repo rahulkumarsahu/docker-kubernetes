7e94fcbb8d6d88bfa4094430676498ba14e49119
Rahul Kumar <30971078+rahulkumarsahu@users.noreply.github.com>
2021-06-02 16:05:08 +0530

    Add files via upload

---
+++ docker/microservice-kubernetes/user-service/src/main/java/com/microservice/user/dto/ResponseTemplate.java
@@ -0,0 +1,16 @@
+package com.microservice.user.dto;
+
+
+import com.microservice.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplate {
+
+    private User user;
+    private Department department;
+}
